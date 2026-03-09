315064b5b9788490cbbd2d4c8c49fe65cb61037d
datthanh9989 <datthanh17092005kk@gmail.com>
2026-03-09 17:43:24 +0700

    48

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,16 +3,22 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vn.hoidanit.laptopshop.service.UserServive;
+
 
 
 
 @Controller
 public class UserController {
+    private UserServive userServive;
+    
+    public UserController(UserServive userServive) {
+        this.userServive = userServive;
+    }
 
     @RequestMapping("/")
-    
-    
     public String getHomepage(){
+        String test = this.userServive.handleHello();
         return "eric.html";
     }
 }
