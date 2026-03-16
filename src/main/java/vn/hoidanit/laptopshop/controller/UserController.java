7acfdf2d84f7d9307368f2c61f0dd7c3c5255724
datthanh9989 <datthanh17092005kk@gmail.com>
2026-03-16 21:59:15 +0700

    61

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -25,6 +27,7 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model){
+        List<User> arrUser = this.userServive.getAllUserByEmail("1@gmail.com");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package vn.hoidanit.laptopshop.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import vn.hoidanit.laptopshop.domain.User;
 
 
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
     User save(User eric);
+    List <User> findOneByEmail(String email);
 }
