d7e8676f38dc0b15cf4431140191c5bd5c23e2c8
datthanh9989 <datthanh17092005kk@gmail.com>
2026-03-30 22:24:01 +0700

    91

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -1,5 +1,9 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -9,79 +13,91 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.servlet.ServletContext;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.UserRepository;
+import vn.hoidanit.laptopshop.service.UploadService;
 import vn.hoidanit.laptopshop.service.UserService;
-
-
-
-
 
 @Controller
 public class UserController {
     private final UserRepository userRepository;
     private final UserService userService;
-    
-    public UserController(UserService userService, UserRepository userRepository) {
+    private final UploadService uploadService;
+
+    public UserController(UserService userService, UserRepository userRepository, UploadService uploadService) {
         this.userService = userService;
         this.userRepository = userRepository;
+        this.uploadService = uploadService;
     }
 
     @RequestMapping("/")
-    public String getHomePage(Model model){
+    public String getHomePage(Model model) {
         List<User> arrUser = this.userService.getAllUserByEmail("1@gmail.com");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
+
     @RequestMapping("/admin/user")
-    public String getUserPage(Model model){
+    public String getUserPage(Model model) {
         List<User> users = this.userService.getAllUser();
         model.addAttribute("user1", users);
         return "admin/user/show";
     }
+
     @RequestMapping("/admin/user/{id}")
-    public String getUserDetailPage(Model model, @PathVariable long id){
+    public String getUserDetailPage(Model model, @PathVariable long id) {
         User user = this.userService.getUserById(id);
         model.addAttribute("user", user);
         model.addAttribute("id", id);
         return "admin/user/detail";
     }
-    @RequestMapping("/admin/user/create")
-    public String getCreateUserPage(Model model){
+
+    @GetMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit){
-        this.userService.handleSaveUser(hoidanit);
+
+    @PostMapping("/admin/user/create")
+    public String createUserPage(Model model,
+            @ModelAttribute("newUser") User hoidanit,
+            @RequestParam("hoidanitFile") MultipartFile file) {
+        this.uploadService.handleSaveUploadFile(file, "avatar");
+        // this.userService.handleSaveUser(hoidanit);
         return "redirect:/admin/user";
     }
+
     @RequestMapping("/admin/user/update/{id}")
-    public String getUpdateUserPage(Model model, @PathVariable long id){
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
         User currentUser = this.userService.getUserById(id);
         model.addAttribute("newUser", currentUser);
         return "admin/user/update";
     }
+
     @PostMapping("/admin/user/update")
     public String postUpdateUser(Model model, @ModelAttribute("newUser") User hoidanit) {
         User currentUSer = this.userService.getUserById(hoidanit.getId());
-        if(currentUSer != null){
+        if (currentUSer != null) {
             currentUSer.setAddress(hoidanit.getAddress());
             currentUSer.setFullName(hoidanit.getFullName());
             currentUSer.setPhone(hoidanit.getPhone());
             this.userService.handleSaveUser(currentUSer);
         }
-        
+
         return "redirect:/admin/user";
     }
+
     @GetMapping("/admin/user/delete/{id}")
-    public String getDeleteUserPage(Model model, @PathVariable long id){
-        model.addAttribute("id",id);
-        model.addAttribute("newUser",new User());
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        model.addAttribute("newUser", new User());
         return "admin/user/delete";
     }
+
     @PostMapping("/admin/user/delete")
     public String postDeleteUser(Model model, @ModelAttribute("newUser") User eric) {
         this.userService.deleteAUser(eric.getId());
@@ -90,14 +106,14 @@ public class UserController {
 }
 // @RestController
 // public class UserController {
-//     private UserService userService;
-    
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
+// private UserService userService;
 
-//     @GetMapping("")
-//     public String getHomepage(){
-//         return this.userService.handleHello();
-//     }
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomepage(){
+// return this.userService.handleHello();
+// }
 // }
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -15,7 +15,7 @@ import jakarta.persistence.Table;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String password;
@@ -27,52 +27,82 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
-    
-    @OneToMany(mappedBy= "user")
+
+    @OneToMany(mappedBy = "user")
     private List<Order> orders;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 
     public void setId(long id) {
         this.id = id;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public long getId() {
         return id;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public String getAddress() {
         return address;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public String getAvatar() {
         return avatar;
     }
+
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
